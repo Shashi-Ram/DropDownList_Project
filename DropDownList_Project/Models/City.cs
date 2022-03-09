9fe762907b2a317344c4c3688a509c392bf1a5e2
Shashi Ram <bharramshashi@outlook.com>
2022-03-09 12:41:03 +0530

    Add project files.

---
+++ DropDownList_Project/Models/City.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DropDownList_Project.Models
+{
+    public class City 
+    {
+        public int Id { get; set; }
+        public int CityName { get; set; }
+        public State State { get; set; }
+    }
+}
---
+++ DropDownList_Project/Models/State.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DropDownList_Project.Models
+{
+    public class State
+    {
+        public int Id { get; set; }
+        public int StateName { get; set; }
+        public Country Country { get; set; }
+    }
+}
